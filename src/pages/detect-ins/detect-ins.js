dec60b2fd87daf4e4ec2e8ce1e94bd3fda976009
xupan <18233566286@163.com>
2019-06-19 16:54:47 +0800

    s

---
+++ src/pages/detect-ins/detect-ins.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import detectIns from './detect-ins.vue'
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: h => h(detectIns)
+})
---
+++ src/pages/detect-ins/detect-ins.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="app">
+       <screen></screen>
+    </div>
+</template>
+
+<script>
+import screen from 'components/detect-ins/screen'
+export default {
+  name: 'app',
+  components: { screen }
+}
+</script>
+
+<style>
+#app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+}
+</style>
