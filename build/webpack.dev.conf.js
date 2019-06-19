f221e0f0d0805d91da881337a7bdd59a8829e013
xupan <18233566286@163.com>
2019-06-19 16:56:10 +0800

    d

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -3,7 +3,6 @@ var webpack = require('webpack')
 var config = require('../config')
 var merge = require('webpack-merge')
 var baseWebpackConfig = require('./webpack.base.conf')
-var HtmlWebpackPlugin = require('html-webpack-plugin')
 var FriendlyErrorsPlugin = require('friendly-errors-webpack-plugin')
 
 // add hot-reload related code to entry chunks
