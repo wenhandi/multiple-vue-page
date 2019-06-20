d24268dab7ba7d4b658cdcd5860dc63a3a7c054c
xupan <18233566286@163.com>
2019-06-20 11:23:06 +0800

    demo

---
+++ src/components/detect-ins/qrCode.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div>设备二维码</div>
+    <div><img class="" src="../../assets/placeholder.png"></div>
+    <div>设备二维码在柜体侧面的金属小牌上</div>
+    <div>工控二维码</div>
+    <div><img src="../../assets/placeholder.png"></div>
+    <div>工控二维码会在柜子重启成功后显示</div>
+    <div class="hor-center">设备编码和工控二维码</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "cell",
+  data() {
+    return {
+      
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
--- src/pages/detect-ins/detect-ins.vue
+++ src/pages/detect-ins/detect-ins.vue
@@ -1,24 +1,52 @@
 <template>
-    <div id="app">
-       <screen></screen>
-    </div>
+  <div id="app">
+    <!-- 屏幕检测 -->
+    <screen></screen>
+    <qr-code></qr-code>
+    <qr-code></qr-code>
+    <screen></screen>
+    <qr-code></qr-code>
+    <qr-code></qr-code>
+    <qr-code></qr-code>
+  </div>
 </template>
 
 <script>
-import screen from 'components/detect-ins/screen'
+import screen from "components/detect-ins/screen";
+import qrCode from "components/detect-ins/qrCode";
 export default {
-  name: 'app',
-  components: { screen }
-}
+  name: "app",
+  components: { screen, qrCode, qrCode, qrCode, qrCode, qrCode, }
+};
 </script>
 
 <style>
 #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
+  font-size: 18px;
+  margin-left: 40px;
+  margin-right: 40px;
+}
+.ins-tip{
+  font-size: 14px;
+  color: #777777;
+}
+.hor-center {
+  text-align: center;
+}
+.ins-img{
+  width: 100%;
+}
+.between-flex{
+  display: flex;
+  justify-content: space-between;
+}
+.box-49{
+  width: 49%;
+}
+.margin-b-30{
+  margin-bottom: 30px;
+}
+.part{
+  margin-bottom: 80px;
 }
 </style>
