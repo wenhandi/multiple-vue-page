f53ef9db14c10a5565e74059cc089d2618d2cd17
xupan <18233566286@163.com>
2019-06-20 11:50:31 +0800

    demo

---
+++ src/components/detect-ins/result.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div class="part">
+      <div class="margin-b-30">屏幕正常</div>
+      <div><img class="ins-img" alt="屏幕" src="../../assets/placeholder.png"></div>
+    </div>
+    <div class="part">
+      <div class="margin-b-30">屏幕异常</div>
+      <div class="between-flex">
+        <div class="box-49"><img class="ins-img" src="../../assets/placeholder.png"></div>
+        <div class="box-49"><img class="ins-img" src="../../assets/placeholder.png"></div>
+      </div>
+      <div class="ins-tip">1.屏幕黑屏</div>
+      <div class="ins-tip">2.屏幕花屏或显示有空隙</div>
+    </div>
+    <div class="hor-center">屏幕检测</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "cell",
+  data() {
+    return {
+      
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
